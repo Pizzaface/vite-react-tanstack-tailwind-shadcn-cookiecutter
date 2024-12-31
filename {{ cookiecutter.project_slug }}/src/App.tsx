3f9c299ba86c0655bd4fdc300ed0655d2f3e7ca0
Jordan <pizzaface@users.noreply.github.com>
2024-12-30 21:35:49 -0500

    Fixes for lowercase naming

--- {{ cookiecutter.project_slug }}/src/App.tsx
+++ {{ cookiecutter.project_slug }}/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "@/Index";
+import HomePage from "@/index.tsx";
 import "@/globals.css";
 
 const queryClient = new QueryClient();
